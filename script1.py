c966a7b87e8a7bd13554691c6b0098f1af8fc6b8
SRCinVA <scitarella@hotmail.com>
2021-03-29 21:49:59 -0400

    added some simple html

--- script1.py
+++ script1.py
@@ -1,13 +1,18 @@
-from flask import Flask # dont know why this returns a warning
+from flask import Flask, render_template # dont know why this returns a warning
 
 app = Flask(__name__)  # create a variable to store Flask instance.
                         # i.e., you are instantiating the Flask object.
                         # __name__ is a special variable that gets as a value the name of the python script.
                         # executed alone, it defaults to the name main.
                         # if it were not "main," it would just be "script1" (the file name as is)
-@app.route('/') # a decorator for the route to the homepage
+
+@app.route('/')  # a decorator for the route to the homepage
 def home():
-    return "Website content goes here!"
+    return render_template("home.html")
+
+@app.route('/about/') # a decorator for the route to the homepage
+def about(): # the functions have to be different for each method
+    return "This is the About page."
 
 if __name__ == "__main__": # Python assigns the name of "main" to the file.
     app.run(debug=True) # means "if name == main, then go ahead and run the app."
